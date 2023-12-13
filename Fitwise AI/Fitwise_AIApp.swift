ae43d96f595a22843b07fee5331d70b54c573c8a
Keshav Khaneja <me@keshav.me>
2023-12-14 01:14:25 +0530

    Initial Commit

---
+++ Fitwise AI/Fitwise_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitwise_AIApp.swift
+//  Fitwise AI
+//
+//  Created by Keshav Khaneja on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Fitwise_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
