e161289c1ae5d552131c3603e2051d7ccd654dbc
Keshav Khaneja <me@keshav.me>
2023-12-24 04:41:16 +0530

    EOD & EOW commit! Going to crush this on monday now. All the other fitness apps -- watch out; here I come. And to lovely SF too. But first, I need $100K in the bank account.

---
+++ Fitwise AI/ViewModels/WorkoutViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WorkoutViewModel.swift
+//  Fitwise AI
+//
+//  Created by Keshav Khaneja on 12/23/23.
+//
+
+import Foundation
---
+++ Fitwise AI/Views/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  Fitwise AI
+//
+//  Created by Keshav Khaneja on 12/24/23.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
